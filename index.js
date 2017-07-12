eb37905e3b145874ddd0bd5374afe7b2df316b17
zhuyian92 <workyianzhu@gmail.com>
2017-07-12 10:29:00 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ restService.post('/hook', function (req, res) {
                     }
                     var url = "https://api.worldweatheronline.com/premium/v1/weather.ashx?format=json&num_of_days=1&" +
                                 "q=" + city + "&key=9b586ac440a244c0bbd205511171107&date=" + date;
-                    var xmlhttp = new XMLHttpRequest();
+                    // var xmlhttp = new XMLHttpRequest();
                     // xmlhttp.onreadystatechange = function() {
                     //     if (this.readyState == 4 && this.status == 200) {
                     //         var response = JSON.parse(this.responseText);
